814e4b13b292022f3cb8038fca40102a01e4e6ff
Allen Kung <ak2253@njit.edu>
2020-11-04 17:24:09 +0000

    Add server.py to test initialization of table Users and Transaction.

---
+++ server.py
@@ -0,0 +1,26 @@
+#This file was created to test initialization of the databases
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
+import flask
+import flask_sqlalchemy
+import flask_socketio
+
+app = flask.Flask(__name__)
+socketio = flask_socketio.SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
+dotenv_path = join(dirname(__file__), "key.env")
+load_dotenv(dotenv_path)
+DATABASE_URL = os.environ["DATABASE_URL"]
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+
+db = flask_sqlalchemy.SQLAlchemy(app)
+db.init_app(app)
+db.app = app
+
+import users
+import transaction
+
+db.session.commit()
