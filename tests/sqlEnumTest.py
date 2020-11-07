8735842053a0dde5019d9658c3a9fe4bc793a677
Allen Kung <ak2253@njit.edu>
2020-11-07 22:21:13 +0000

    Add tests folder to hold files for unit testing and add test file to test enum classes.

---
+++ tests/sqlEnumTest.py
@@ -0,0 +1,38 @@
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import server.models as table
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+
+class SqlEnumTest(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params_user = [
+            {KEY_INPUT: "google", KEY_EXPECTED: "GOOGLE"},
+            {KEY_INPUT: "password", KEY_EXPECTED: "PASSWORD"},
+        ]
+        self.success_test_params_transaction = [
+            {KEY_INPUT: "transfer", KEY_EXPECTED: "TRANSFER"},
+            {KEY_INPUT: "coinfilp", KEY_EXPECTED: "COINFILP"},
+        ]
+
+    def test_success_login_type(self):
+        for test_case in self.success_test_params_user:
+            result = table.user.Login_Type(test_case[KEY_INPUT]).name
+            expected = test_case[KEY_EXPECTED]
+            self.assertEqual(expected, result)
+
+    def test_success_activity_type(self):
+        for test_case in self.success_test_params_transaction:
+            result = table.transaction.Activity_Type(test_case[KEY_INPUT]).name
+            expected = test_case[KEY_EXPECTED]
+            self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
