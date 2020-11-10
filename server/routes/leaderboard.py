937076bacff54f8e9f7639da7da7831a9812e843
David Kim <d@vid.kim>
2020-11-10 10:53:08 -0500

    Add pylint and fix lint errors

--- server/routes/leaderboard.py
+++ server/routes/leaderboard.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql import func
 from server import app, db
 
 from server.models.transaction import Transaction
@@ -7,10 +8,10 @@ from server.models.user import User
 @app.route("/leaderboard")
 def get_leader_board():
     all_transactions = (
-        db.session.query(User.name, db.func.sum(Transaction.ticket_amount))
+        db.session.query(User.name, func.sum(Transaction.ticket_amount))
         .outerjoin(Transaction, User.id == Transaction.user_id)
         .group_by(User.name)
-        .order_by(db.func.sum(Transaction.ticket_amount).desc())
+        .order_by(func.sum(Transaction.ticket_amount).desc())
         .all()
     )
     return {"transactions": all_transactions}
