b79d93eed0059e6b56f495fc9bcd4a3aec1b31f0
David Kim <d@vid.kim>
2020-11-10 11:55:35 -0500

    Fix travis warning

--- test_input.py
+++ test_input.py
@@ -3,13 +3,18 @@ import sys
 import flask
 import flask_sqlalchemy
 import flask_socketio
-from server import app,db
+from server import app, db
 
 
-from server.models.transaction import Transaction
+from server.models.transaction import Transaction, ActivityType
 from server.models.user import User
 
 
-
-db.session.add(Transaction(3, 50, "coinflip"))
+user = User(oauth_id="1", name="David", username="David")
+db.session.add(user)
+db.session.commit()
+transaction = Transaction(
+    user_id=user.id, ticket_amount=50, activity=str(ActivityType.COINFILP)
+)
+db.session.add(transaction)
 db.session.commit()
