bb8ef1d258bb1232b2c59c6f6a5bbf367c53577a
Allen Kung <ak2253@njit.edu>
2020-11-11 18:11:58 -0500

    Add <tbody> to table in Tickethistory.

--- src/Tickethistory.tsx
+++ src/Tickethistory.tsx
@@ -23,21 +23,23 @@ function Tickethistory() {
       });
   }, []);
   return (
-    <table>
-      <tr>
-        <th> </th>
-        <th>Date</th>
-        <th>Activity</th>
-        <th>Amount</th>
-      </tr>
-      {tHistory.map((row, index) => (
-        <tr key={row.id}>
-          <td>{index + 1}</td>
-          <td>{row.datetime}</td>
-          <td>{row.activity}</td>
-          <td>{row.amount}</td>
+    <table className="transaction-history-table">
+      <tbody className="table-body">
+        <tr>
+          <th> </th>
+          <th>Date</th>
+          <th>Activity</th>
+          <th>Amount</th>
         </tr>
-      ))}
+        {tHistory.map((row, index) => (
+          <tr key={row.id}>
+            <td>{index + 1}</td>
+            <td>{row.datetime}</td>
+            <td>{row.activity}</td>
+            <td>{row.amount}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
