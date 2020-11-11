c54c207fb8119275ad40f76440507766b4d34e1e
EC2 Default User <ec2-user@ip-172-31-94-217.ec2.internal>
2020-11-11 22:12:04 +0000

    Coinflip Testing almost done

--- tests/CoinflipTest.py
+++ tests/CoinflipTest.py
@@ -4,12 +4,12 @@ import sys
 import unittest.mock as mock
 from alchemy_mock.mocking import AlchemyMagicMock
 sys.path.append(os.path.abspath(os.path.join("./server/routes/")))
-import leaderboard
+import coinflip
 
-class LeaderboardTest(unittest.TestCase):
-    def test_success_leaderboard(self):
+class CoinflipTest(unittest.TestCase):
+    def test_success_coinflip(self):
         with mock.patch("db", AlchemyMagicMock()):
-            response = leaderboard.get_leader_board()
+            response = coinflip.coinflip()
             self.assertIs(type(response), dict)
 
 if __name__ == "__main__":
