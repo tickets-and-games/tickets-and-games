1870c1b73e9891cd496e6a377a09b2f9a5be65d6
Mohammad <mhk8@njit.edu>
2020-11-12 21:53:11 +0000

    fixed lint errors and added google dependency

--- src/GoogleButton.tsx
+++ src/GoogleButton.tsx
@@ -1,39 +1,24 @@
-import React from "react";
-import {
-  GoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from "react-google-login";
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
 
-interface Props {
-  setUserId: (userId: string) => void;
-  setLoggedIn: (isLoggedIn: boolean) => void;
+function googleLogin(googleUser: any) {
+  fetch('/api/login/oauth', {
+    method: 'POST',
+    headers: new Headers({ 'content-type': 'application/json' }),
+    mode: 'no-cors',
+    body: JSON.stringify({ tokenId: googleUser?.tokenId }),
+  });
 }
 
-export default function Login(props: Props) {
-  const googleLogin = (
-    googleUser: GoogleLoginResponse | GoogleLoginResponseOffline
-  ) => {
-    fetch("/api/login/oauth", {
-      method: "POST",
-      headers: new Headers({ "content-type": "application/json" }),
-      mode: "no-cors",
-      body: JSON.stringify({ googleUser: googleUser }),
-    });
-  };
-
+export default function Login() {
   return (
     <div className="LoginBox">
       <div className="Login">
         <h1>Login to continue</h1>
         <GoogleLogin
-          clientId="318870608375-efi0el1rcp01sjik3iterm017i06tm9j.apps.googleusercontent.com"
+          clientId="154638215001-5tdj4ttljsh2c3m5uojmq7nrruock21s.apps.googleusercontent.com"
           buttonText="Login"
-          onSuccess={(
-            googleUser: GoogleLoginResponse | GoogleLoginResponseOffline
-          ) => {
-            googleLogin(googleUser);
-          }}
+          onSuccess={googleLogin}
           cookiePolicy="single_host_origin"
           isSignedIn
         />
