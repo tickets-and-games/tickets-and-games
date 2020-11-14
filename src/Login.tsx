9efebdebafaf764db385d1b36111ea9c3d5919ba
David Kim <d@vid.kim>
2020-11-13 22:17:22 -0500

    Hide menu if not logged in

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
 
-function googleLogin(googleUser: any) {
-  fetch('/api/login/oauth', {
-    method: 'POST',
-    headers: new Headers({ 'content-type': 'application/json' }),
-    mode: 'no-cors',
-    body: JSON.stringify({ token: googleUser?.tokenId }),
-  });
+interface Props {
+  setLoggedIn: (isLoggedIn: boolean) => void;
 }
 
-export default function Login() {
+export default function Login(props: Props) {
+  const { setLoggedIn } = props;
+
+  function googleLogin(googleUser: any) {
+    setLoggedIn(true);
+    fetch('/api/login/oauth', {
+      method: 'POST',
+      headers: new Headers({ 'content-type': 'application/json' }),
+      mode: 'no-cors',
+      body: JSON.stringify({ token: googleUser?.tokenId }),
+    });
+  }
+
   return (
     <div className="LoginBox">
       <div className="Login">
