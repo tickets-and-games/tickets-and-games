207fbfb63a7ff4c87b4a9ed203fe0562c4230a3e
David Kim <d@vid.kim>
2020-11-17 17:59:53 -0500

    Make flask db migration render as batch
    sqlite doesn't support ALTER command so we have to enable render as batch.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
-from server import app, db
 from flask_migrate import Migrate
+from server import app, db
 
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 if __name__ == "__main__":
     app.run(
