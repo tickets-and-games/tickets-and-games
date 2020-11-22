4f093e785d7be062c977a53cd810a7f88f9d9737
speshori <sap229@njit.edu>
2020-11-22 17:40:09 +0000

    one fix

--- server/__init__.py
+++ server/__init__.py
@@ -37,5 +37,4 @@ def create_app(config):
         app.register_blueprint(routes.ticket_bp)
         app.register_blueprint(routes.blackjack_bp)
         app.register_blueprint(routes.skiball_bp)
-    
     return app
