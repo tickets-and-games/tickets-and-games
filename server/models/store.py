a541db11250ef4e9c63fe91ffe63c95e32a6ad76
ak2253 <ak2253@njit.edu>
2020-11-26 16:20:11 -0500

    Add new column to Store table and made nessessary changes.

--- server/models/store.py
+++ server/models/store.py
@@ -4,6 +4,7 @@ from server import db
 class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    item_type = db.Column(db.Integer)
     item_group = db.Column(db.Integer)
     price = db.Column(db.Integer)
     limit = db.Column(db.Integer)
--- server/routes/store.py
+++ server/routes/store.py
@@ -7,36 +7,36 @@ from server import db
 
 store_bp = Blueprint("store_bp", __name__, url_prefix="/api/store")
 
-def valid_funds(user_id, item_id):
+def valid_funds(user_id, item_type):
     total_tickets = (
         db.session.query(func.sum(Transaction.ticket_amount))\
         .filter(Transaction.user_id == user_id)
         .scalar()
     )
-    item_price = Store.query.filter_by(id=item_id).first().price
+    item_price = Store.query.filter_by(item_type=item_type).first().price
     if total_tickets is None or item_price is None:
         return False
     return total_tickets >= item_price
 
-def item_available(user_id, item_id):
-    item_info = Store.query.filter_by(id=item_id).first()
-    item_query = Item.query.filter_by(user_id=user_id, item_type=item_id).first()
-    if item_query is not None and item_query.count >= item_info.limit:
+def item_available(user_id, item_type, quantity):
+    item_info = Store.query.filter_by(item_type=item_type).first()
+    item_query = Item.query.filter_by(user_id=user_id, item_type=item_type).first()
+    if item_query is not None and item_query.count + quantity > item_info.limit:
         return False
     return True
 
-def make_purchase(user_id, item_id):
-    item_info = Store.query.filter_by(id=item_id).first()
+def make_purchase(user_id, item_type):
+    item_info = Store.query.filter_by(id=item_type).first()
     transaction = Transaction(
         user_id=user_id,
         ticket_amount=item_info.price,
         activity="ticketstore",
     )
     db.session.add(transaction)
-    item_query = Item.query.filter_by(user_id=user_id, item_type=item_id).first()
+    item_query = Item.query.filter_by(user_id=user_id, item_type=item_type).first()
     if item_query is None:
         item = Item(
-            item_type = item_id,
+            item_type = item_type,
             item_group = item_info.item_group,
             user_id = user_id,
         )
@@ -64,13 +64,14 @@ def store_list():
 def buy_item():
     try:
         data = json.loads(request.data)
-        item_id = data["item_id"]
+        item_type = data["item_type"]
+        quantity = data["quantity"]
         user_id = session["user_id"]
-        if not valid_funds(user_id, item_id):
+        if not valid_funds(user_id, item_type):
             return {"success": False, "message": "Insufficient funds"}
-        if not item_available(user_id, item_id):
+        if not item_available(user_id, item_type, quantity):
             return {"success": False, "message": "Reached purchase limit"}
-        make_purchase(user_id, item_id)
+        make_purchase(user_id, item_type)
         return {"success": True}
     except json.decoder.JSONDecodeError:
         return {"error": "Malformed request"}, 400
