5f68ab97bcffb924842713f544bd479b0c007854
David Kim <d@vid.kim>
2020-11-29 04:51:05 -0500

    Add redux and app wide alert

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer);
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  rootElement,
 );
 
 // If you want to start measuring performance in your app, pass a function
