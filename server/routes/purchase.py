60314043f754c089a0187b8eeaae9cc933256a3a
David Kim <d@vid.kim>
2020-11-29 07:04:23 -0500

    Fix webhook methods

--- server/routes/purchase.py
+++ server/routes/purchase.py
@@ -12,6 +12,7 @@ from server.routes.decorators import login_required, stripe_required
 purchase_bp = Blueprint("purchase_bp", __name__, url_prefix="/api/purchase")
 
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY", "DEFAULT_KEY")
+STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET", "DEFAULT_SECRET")
 
 
 @purchase_bp.route("/payment-intent")
@@ -23,35 +24,42 @@ def create_payment_intent():
     payment_intent = stripe.PaymentIntent.create(
         amount=1000,
         currency="usd",
-        payment_method_types=["card"],
         receipt_email=user.email,
     )
 
     return payment_intent
 
 
-@purchase_bp.route("/complete")
+@purchase_bp.route("/complete", methods=["POST"])
 @stripe_required
 def payment_complete():
+    payload = request.get_data()
     event_data = json.loads(request.data)
+    sig_header = request.headers.get("STRIPE_SIGNATURE")
 
-    event_object = event_data["object"]
-    status = event_object["status"]
-    client_secret = event_object["client_secret"]
+    data = event_data["data"]
+    event_object = data["object"]
     amount = event_object["amount"]
     email = event_object["receipt_email"]
 
-    if status != "succeeded":
-        return {"error": "Payment not successful"}, 402
+    try:
+        if (
+            os.getenv("DEBUG", "").lower() != "true"
+        ):  # Skip signiture check on debug mode
+            stripe.Webhook.construct_event(payload, sig_header, STRIPE_ENDPOINT_SECRET)
+    except ValueError:
+        # invalid payload
+        return "Invalid payload", 400
+    except stripe.error.SignatureVerificationError:
+        # invalid signature
+        return "Invalid signature", 400
 
-    if client_secret != stripe.api_key:
-        return {"error": "Wrong client secret"}, 400
-
-    user = User.query.filter_by(email=email)
-    transaction = Transaction(
-        uesr_id=user.id, ticket_amount=amount * 100, activity="Ticket purchase"
-    )
-    db.session.add(transaction)
-    db.session.commit(transaction)
+    user = User.query.filter_by(email=email).first()
+    if user is not None:
+        transaction = Transaction(
+            user_id=user.id, ticket_amount=amount * 100, activity="Ticket purchase"
+        )
+        db.session.add(transaction)
+        db.session.commit()
 
     return {"success": True}
