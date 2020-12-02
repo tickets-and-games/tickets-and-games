7ff82207e253a909eafb9b7285c503eabc5893f4
Mohammad Khan <67294019+mhk8@users.noreply.github.com>
2020-12-02 16:22:47 -0500

    Update oauth_login_test.py

--- tests/oauth_login_test.py
+++ tests/oauth_login_test.py
@@ -1,18 +1,18 @@
-from config import Config
-from server import db, create_app
-import unittest
 import unittest.mock as mock
 import json
 import os
 import sys
-import flask_sqlalchemy
+
+from server import db
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from server.utils.database_test import DatabaseTest
+from server.models.user import User
 
-KEY_INPUT = "input"
-KEY_EXPECTED = "expected"
-
+KEY_INPUT1 = "input"
+KEY_EXPECTED1 = "expected"
+KEY_DATABASE1= "database"
 
 def mocked_token_info(sub):
     return {
@@ -32,10 +32,6 @@ def mocked_token_info(sub):
     }
 
 
-def mocked_empty_query(_email):
-    return None
-
-
 class MockedTransData:
     id = 1
     datetime = "Tue, 10 Nov 2020 18:19:43 GMT"
@@ -43,22 +39,35 @@ class MockedTransData:
     ticket_amount = 50
 
 
-class OauthLoginTest(unittest.TestCase):
+class OauthLoginTest(DatabaseTest):
     def setUp(self):
-        config = Config()
-        config.SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-        self.app = create_app(config)
-        self.client = self.app.test_client()
-        with self.app.app_context():
-            db.drop_all()
-            db.create_all()
-            db.session.commit()
-
-        self.client = self.app.test_client()
+        super().setUp()
         self.success_test_params_user = [
             {
                 KEY_INPUT: json.dumps({"token": "google_oauth_token"}),
+                KEY_DATABASE: User (
+                    oauth_id="google",
+                    name="Test User",
+                    username="Test",
+                    email="testuser@gmail.com"
+                )
             },
+            {
+                KEY_INPUT: json.dumps({"token": "google_oauth_token"}),
+                KEY_DATABASE: User (
+                    oauth_id="google",
+                    name="Test User",
+                    email="testuser@gmail.com"
+                )
+            },
+            {
+                KEY_INPUT: json.dumps({"token": "google_oauth_token"}),
+                KEY_DATABASE: User (
+                    oauth_id="google",
+                    name="Other user",
+                    email="otheruser@gmail.com"
+                )
+            }
         ]
         self.fail_test_params_user = [
             {
@@ -71,26 +80,25 @@ class OauthLoginTest(unittest.TestCase):
             },
         ]
 
-    def tearDown(self):
-        with self.app.app_context():
-            db.drop_all()
-            db.session.commit()
+    def user_table_reset(self):
+        db.session.query(User).delete()
+        db.session.commit()
 
     def test_success_transaction_history(self):
         for test_case in self.success_test_params_user:
-            with self.client as client:
+            with self.app.app_context():
+                self.user_table_reset()
+                db.session.add(test_case[KEY_DATABASE])
+                db.session.commit()
                 with mock.patch("server.routes.user.get_token_info", mocked_token_info):
-                    with mock.patch(
-                        "server.routes.user.query_user", mocked_empty_query
-                    ):
-                        response = client.post(
-                            "/api/login/oauth", data=test_case[KEY_INPUT]
-                        )
-                        self.assertIn("success", response.json)
+                    response = self.client.post(
+                        "/api/login/oauth", data=test_case[KEY_INPUT]
+                    )
+                    self.assertIn("success", response.json)
 
     def test_fail_oauth_login(self):
         for test_case in self.fail_test_params_user:
-            with self.client as client:
-                response = client.post("/api/login/oauth", data=test_case[KEY_INPUT])
+            with self.app.app_context():
+                response = self.client.post("/api/login/oauth", data=test_case[KEY_INPUT])
                 self.assertIn("error", response.json)
                 self.assertEqual(test_case[KEY_EXPECTED], response.json["error"])
