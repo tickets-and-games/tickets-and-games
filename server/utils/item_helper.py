1724df5c0466bb72512638f1840d87b78edf8b10
David Kim <d@vid.kim>
2020-12-08 11:21:12 -0500

    Change default username color to white

--- server/utils/item_helper.py
+++ server/utils/item_helper.py
@@ -2,19 +2,21 @@ from rfc3987 import parse
 from server.models import Item, User, Login, Store
 from server import db
 
+
 def item_group_by_user_id(user_id, item_group):
     return (
         db.session.query(Item)
-        .filter(Item.user_id==user_id, Item.item_group==item_group)
+        .filter(Item.user_id == user_id, Item.item_group == item_group)
         .all()
     )
 
+
 def handle_text_color(user_id, item_type):
-    if item_type == '':
-        item_type=-1
+    if item_type == "":
+        item_type = -1
     query = (
         db.session.query(Item)
-        .filter(Item.user_id==user_id, Item.item_group==101)
+        .filter(Item.user_id == user_id, Item.item_group == 101)
         .all()
     )
     for color in query:
@@ -24,34 +26,26 @@ def handle_text_color(user_id, item_type):
             color.active = False
     db.session.commit()
 
+
 def get_color_name(item_type):
     query = (
         db.session.query(Store)
-        .filter(Store.item_group==101, Store.id==item_type)
+        .filter(Store.item_group == 101, Store.id == item_type)
         .first()
     )
     if query:
         return query.name.split(" ", 1)[0]
     return "Invalid"
 
+
 def handle_username_change(user_id, username):
-    query = (
-        db.session.query(User)
-        .filter(User.username==username)
-        .all()
-    )
+    query = db.session.query(User).filter(User.username == username).all()
     if not query:
-        user_profile = (
-            db.session.query(User)
-            .filter(User.id==user_id)
-            .first()
-        )
+        user_profile = db.session.query(User).filter(User.id == user_id).first()
         temp_username = user_profile.username
         user_profile.username = username
         login_profile = (
-            db.session.query(Login)
-            .filter(Login.username==temp_username)
-            .first()
+            db.session.query(Login).filter(Login.username == temp_username).first()
         )
         if login_profile is not None:
             login_profile.username = username
@@ -59,27 +53,25 @@ def handle_username_change(user_id, username):
         return True
     return False
 
+
 def handle_profile_image(user_id, image_url):
     try:
         parse(image_url, rule="IRI")
     except ValueError:
         return False
-    query = (
-        db.session.query(User)
-        .filter(User.id==user_id)
-        .first()
-    )
+    query = db.session.query(User).filter(User.id == user_id).first()
     query.image_url = image_url
     db.session.commit()
     return True
 
+
 def get_current_color(user_id):
     query = (
         db.session.query(Item)
-        .filter(Item.user_id==user_id, Item.item_group==101)
+        .filter(Item.user_id == user_id, Item.item_group == 101)
         .all()
     )
     for color in query:
         if color.active:
             return get_color_name(color.item_type).lower()
-    return "black"
+    return "white"
