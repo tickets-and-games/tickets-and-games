84d2736ebae135c2a77a7b4cfc4b3019d4b6059e
Samir Peshori <32985473+speshori@users.noreply.github.com>
2020-12-08 20:42:44 -0500

    Fixed Spelling Mistake

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -184,7 +184,7 @@ export default function Home() {
         </Typography>
         <br />
         <Typography variant="h6">
-          We are lot of 4th year CS majors from NJIT, with ambitions like these to create even more
+          We are a lot of 4th year CS majors from NJIT, with ambitions like these to create even more
           PC games (not that we play enough). This keeps us busy and engaged as we develop,
           but also as we play our works of art (our definition might differ from
           yours *cough cough*). The opportunity to learn more about our trade
