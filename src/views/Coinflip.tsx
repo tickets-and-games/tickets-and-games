25e7e0d0788ba4ae44cdeea1adab91e579e581be
Mhk8 <mhk8@njit.edu>
2020-12-09 14:13:52 +0800

    frontend change

--- src/views/Coinflip.tsx
+++ src/views/Coinflip.tsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
-import './Styles.css';
 import {
-  Box, Button, ButtonGroup, Paper, Typography,
+  Box,
+  Button,
+  ButtonGroup,
+  Paper,
+  Typography,
+  Input,
+  FormControl,
 } from '@material-ui/core';
+import { useStyles } from '../styles';
+import CoinflipImage from '../assets/image/coin.gif';
 
 function Coinflip() {
+  const classes = useStyles();
   const [bet, setBet] = useState(0);
+  const [value, setValue] = useState(0);
   const [message, setMessage] = useState('');
 
   const onChange = (event) => {
-    setBet(event.target.value);
+    let goal = 0;
+    if (event.target.value !== '') {
+      goal = parseInt(event.target.value, 10);
+      setBet(goal);
+      setValue(goal * 2);
+    } else {
+      setBet(0);
+      setValue(0);
+    }
   };
 
   const play = (side: 'Tails' | 'Heads') => {
@@ -48,27 +65,40 @@ function Coinflip() {
 
   return (
     <div className="Coinflip">
-      <Paper className="gradient-border" style={{ background: '#310000', color: 'white' }}>
+      <Paper style={{ background: 'none', color: 'white', boxShadow: 'none' }}>
         <br />
         <Typography variant="h3">
           Coinflip
         </Typography>
         <br />
         <Typography variant="h5"> Enter an amount you would like to wager. </Typography>
-        <Typography variant="h5">If you win, you win your bet amount. If you lose, you lose all your tickets you bet. </Typography>
-        <img src="https://image.tutpad.com/tut/0/53/11_MONEDA.gif" alt="coinflip" className="coinflipgif" style={{ height: '200px', width: '400px' }} />
+        <Typography variant="h5">
+          If you win, you win&nbsp;
+          <span className="golden">{value}</span>
+          tickets. If you lose, you lose&nbsp;
+          <span className="golden">{bet}</span>
+          tickets you bet.
+        </Typography>
+        <img src={CoinflipImage} alt="Coin Flip" className="coinflipgif" />
         <br />
         <Typography variant="h6">Place a Bet in Number of Tickets:</Typography>
         <br />
-        <input type="text" onChange={onChange} />
+        <FormControl fullWidth>
+          <Input
+            id="standard-adornment-amount"
+            className={classes.textField}
+            value={bet}
+            onChange={onChange}
+          />
+        </FormControl>
         <br />
         <br />
         <Typography variant="h6">Choose heads or tails:</Typography>
         <br />
         <Box>
-          <ButtonGroup variant="text" color="default" aria-label="text primary button group">
-            <Button variant="contained" type="button" style={{ right: '10px' }} onClick={onHeads}>Heads</Button>
-            <Button variant="contained" type="button" onClick={onTails}>Tails</Button>
+          <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+            <Button variant="contained" type="button" className={classes.headButton} style={{ right: '10px' }} onClick={onHeads}>Heads</Button>
+            <Button variant="contained" type="button" className={classes.tailButton} onClick={onTails}>Tails</Button>
           </ButtonGroup>
         </Box>
         <br />
