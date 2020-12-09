68ceac637651a706243869ca9dffa58d9de9a90b
David Kim <d@vid.kim>
2020-12-08 22:22:38 -0500

    Fix pylint warnings

--- server/routes/reward.py
+++ server/routes/reward.py
@@ -2,8 +2,6 @@ from datetime import datetime, timedelta
 
 from flask import Blueprint
 
-from server import db
-from server.models import Reward, User
 from server.utils import get_current_user, get_latest_reward_for_user, add_daily_reward
 
 from server.routes.decorators import login_required
@@ -24,5 +22,5 @@ def daily_reward():
         reward = add_daily_reward(user.id)
         return {"success": True, "next_reward": reward.datetime + cooldown}
 
-    next = reward.datetime + cooldown
-    return {"success": False, "next_reward": next}, 400
+    next_reward = reward.datetime + cooldown
+    return {"success": False, "next_reward": next_reward}, 400
